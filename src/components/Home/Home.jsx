1c9a1eafeb707713663e675eaac9244f1a63fbb6
Sai Krishna Koleti <129356513+sai-krishna-koleti@users.noreply.github.com>
2024-04-20 20:29:46 +0530

    Update Home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,20 +1,16 @@
-import React, { useState } from 'react'
-import './homeStyles.css'
-import { WebcamCapture} from '../Webcam/Webcam'
-
+import React, { useState } from 'react';
+import './homeStyles.css';
+import { WebcamCapture } from '../Webcam/Webcam';
 
 const Home = () => {
-
-    const [name, setName] = useState('')
+    const [name, setName] = useState('');
     const [email, setEmail] = useState('');
 
-
     const submitForm = () => {
-        alert("Form submitted");
+        alert(Form submitted with Name: ${name} and Email: ${email});
         setName('');
         setEmail('');
     }
-
 
     return (
         <div className="home-container">
@@ -22,13 +18,15 @@ const Home = () => {
                 <div className="text">
                     <h1>Capture the Image</h1>
                     <form className="form">
-                        <WebcamCapture/>
-                        
+                        <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="Name" />
+                        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
+                        <WebcamCapture />
                         <button type="submit" id="login-button" onClick={(e) => submitForm(e)}>Submit</button>
                     </form>
                 </div>
             </div>
         </div>
-    )
+    );
 }
-export default Home
+
+export default Home;
